4980bdfcea868a8e9457a6f385fda90a2f1cf86a
SDR C137 <119166947+SDRC137@users.noreply.github.com>
2023-02-26 17:36:03 +0000

    Added dynamic data and APi route

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+tasks = [
+  {
+    'id': 1,
+    'title': 'Mates',
+    'Description': 'p123 1,2,3',
+    'fecha': '23/02/2023'
+  },
+  {
+    'id': 2,
+    'title': 'TDR',
+    'Description': 'Index',
+    'fecha': '28/02/2023'
+  },
+  {
+    'id': 3,
+    'title': 'Tecno',
+    'Description': 'questions 1,2,3',
+    'fecha': '1/03/2023'
+  },
+]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', tasks=tasks)
+
+
+@app.route("/api/tasks")
+def tasks_api():
+  return jsonify(tasks)
 
 
 if __name__ == '__main__':
